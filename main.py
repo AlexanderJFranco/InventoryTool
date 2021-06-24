6f2b01d77209e0daa3024c8f4df0b7a4f0bf3999
AlexanderJFranco <AlexanderJFranco@outlook.com>
2021-06-24 14:44:03 -0400

    Full Commit

---
+++ main.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, redirect, url_for
+import json
+from datetime import date
+from pprint import pprint
+todays_date = date.today()
+
+app = Flask(__name__)
+f = open('./db2021.json',)
+data = json.load(f)
+iter = len(data['customer'])
+f.close()
+f = open('./db2021.json', 'w+')
+f.write(json.dumps(data))
+f.close()
+
+@app.route('/instructions')
+def instructions():
+    return render_template ("instructions.html")
+
+@app.route('/')
+def home():
+    f = open('./db2021.json', )
+    data = json.load(f)
+    iter = len(data['customer'])
+    f.close()
+    return render_template ("index.html",data=data,iter=iter)
+
+@app.route('/removeCustomer', methods=['POST','GET'])
+def removeCustomer():
+    def remove(name):
+        with open("./db2021.json", "r+") as file:
+            data = json.load(file)
+            for i in range(len(data['customer'])):
+                if data['customer'][i]['name'] == name:
+                    del data['customer'][i]
+                    break
+            file.seek(0)
+            file.truncate()
+            json.dump(data, file)
+            file.close()
+
+    if request.method=='POST':
+        name = request.form.get('name')
+        remove(name)
+    return render_template ("removeCustomer.html",data=data)
+
+
+@app.route('/addCustomer', methods=['POST','GET'])
+def addCustomer():
+    def add(name):
+        test = {"name": name,"Notes":"", "Balance": "0", "Charge": "0", "JAN": "", "FEB": "", "MAR": "", "APR": "", "MAY": "", "JUN": "", "JUL": "", "AUG": "", "SEP": "", "OCT": "", "NOV": "", "DEC": "", "JANB": "0", "FEBB": "0", "MARB": "0", "APRB": "0", "MAYB": "0", "JUNB": "0", "JULB": "0", "AUGB": "0", "SEPB": "0", "OCTB": "0", "NOVB": "0", "DECB": "0", "JANP": "0", "FEBP": "0", "MARP": "0", "APRP": "0", "MAYP": "0", "JUNP": "0", "JULP": "0", "AUGP": "0", "SEPP": "0", "OCTP": "0", "NOVP": "0", "DECP": "0"}
+        with open("./db2021.json", "r+") as file:
+            data = json.load(file)
+            data['customer'].append(test)
+            file.seek(0)
+            json.dump(data, file)
+            file.close()
+
+
+    if request.method=='POST':
+        name = request.form.get('name')
+        add(name)
+    return render_template ("addCustomer.html",data=data)
+
+@app.route('/Monthly', methods=['POST','GET','ROUTE'])
+def Months():
+    f = open('./db2021.json', )
+    data = json.load(f)
+    iter = len(data['customer'])
+    f.close()
+    def updatedb(month, index, date,list,name,charge,payment,noyes):
+        print(data['customer'][int(index)][month+"P"])
+        if payment!="":
+            data['customer'][int(index)][month +"P"]= float(payment)+float(data['customer'][int(index)][month +"P"])
+
+        if charge =="":
+            charge = data['customer'][int(index)]['Charge']
+        if list !="":
+            data['customer'][int(index)][month] = list
+        if name !="":
+            data['customer'][int(index)]['name'] = name
+        if len(data['customer'][int(index)][month])<5:
+            data['customer'][int(index)][month] +=date
+        elif date !="":
+            data['customer'][int(index)][month] += ", " + date
+
+        temp = data['customer'][int(index)][month].split(' ')
+        if temp!='':
+            monthbalance = len(temp)* float(charge)
+        if data['customer'][int(index)][month+"P"]!="":
+            monthbalance-=float(data['customer'][int(index)][month+"P"])
+        data['customer'][int(index)][month+"B"]= str(monthbalance)
+        data['customer'][int(index)]["Notes"]=notes
+        data['customer'][int(index)]['Balance'] = float(data['customer'][int(index)]['JANB']) + float(data['customer'][int(index)]['FEBB'])+float(data['customer'][int(index)]['MARB']) + float(data['customer'][int(index)]['APRB'])+float(data['customer'][int(index)]['MAYB'])+float(data['customer'][int(index)]['JUNB'])+float(data['customer'][int(index)]['JULB']) + float(data['customer'][int(index)]['AUGB'])+float(data['customer'][int(index)]['SEPB'])+float(data['customer'][int(index)]['OCTB'])+float(data['customer'][int(index)]['NOVB'])+float(data['customer'][int(index)]['DECB'])
+        data['customer'][int(index)]['Charge']=charge
+
+        f = open('./db2021.json', "w+")
+        f.write(json.dumps(data))
+        f.close()
+    if request.method=='POST':
+        notes = request.form.get('note')
+        payment = request.form.get('payment')
+        charge = request.form.get('charge')
+        date = request.form.get('time')
+        name = request.form.get('name')
+        list = request.form.get('list')
+        index = request.form.get('index')
+        test = request.form.get('mon')
+        updatedb(test,index, date,list,name,charge,payment,notes)
+
+
+
+    return render_template ("monthly.html",data=data,iter=len(data['customer']),current=str(todays_date.month))
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ static/monthly.js
@@ -0,0 +1,230 @@
+var monthly='JAN';
+
+
+function ShowFeb() {
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("FEB");
+    x.style.display = "block";
+
+}
+
+function ShowMar() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("MAR");
+    x.style.display = "block";
+
+}
+
+function ShowApr() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("APR");
+    x.style.display = "block";
+
+}
+
+function ShowMay() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("MAY");
+    x.style.display = "block";
+
+}
+
+function ShowJun() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("JUN");
+    x.style.display = "block";
+
+}
+
+function ShowJul() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("JUL");
+    x.style.display = "block";
+
+}
+
+function ShowAug() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("AUG");
+    x.style.display = "block";
+
+}
+
+function ShowSep() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("SEP");
+    x.style.display = "block";
+
+}
+
+function ShowOct() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("OCT");
+    x.style.display = "block";
+
+}
+
+function ShowNov() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("NOV");
+    x.style.display = "block";
+
+}
+
+function ShowDec() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("JAN").style.display = "none";
+  var x = document.getElementById("DEC");
+    x.style.display = "block";
+
+}
+
+
+function menufx(x) {
+  x.classList.toggle("change");
+}
+
+function ShowJan() {
+    document.getElementById("FEB").style.display = "none";
+    document.getElementById("MAR").style.display = "none";
+    document.getElementById("APR").style.display = "none";
+    document.getElementById("MAY").style.display = "none";
+    document.getElementById("JUN").style.display = "none";
+    document.getElementById("JUL").style.display = "none";
+    document.getElementById("AUG").style.display = "none";
+    document.getElementById("SEP").style.display = "none";
+    document.getElementById("OCT").style.display = "none";
+    document.getElementById("NOV").style.display = "none";
+    document.getElementById("DEC").style.display = "none";
+  var x = document.getElementById("JAN");
+    x.style.display = "block";
+
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
